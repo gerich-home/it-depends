a3ca6022ccf6c44092c140ddb7a2618b365b693b
Sergey Gerasimov <gerich.home@gmail.com>
2016-01-30 12:56:29 +0300

    tests

---
+++ specs/basics.js
@@ -0,0 +1,10 @@
+﻿mocha.setup('bdd');
+var expect = chai.expect;
+
+describe('itDepends library', function () {
+
+  it('loads in test runner', function () {
+    expect(itDepends).to.not.be.undefined;
+  });
+
+});
