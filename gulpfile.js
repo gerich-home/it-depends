903635d2adedd06146a8d92429f41fbd1706290b
sergey-gerasimov <sergey.gerasimov@exigenservices.com>
2016-02-01 11:14:10 +0300

    test task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 ﻿var gulp = require('gulp');
+var mochaPhantomJS = require('gulp-mocha-phantomjs');
 
 gulp.task('default', function() {
-	
+});
+
+gulp.task('test', function () {
+  return gulp
+    .src('runner.html')
+    .pipe(mochaPhantomJS());
 });
