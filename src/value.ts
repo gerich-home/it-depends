42e5560dae28838cc2112b48405bcc2d6641a4ee
sergey-gerasimov <sergey.gerasimov@exigenservices.com>
2016-03-14 10:44:51 +0300

    changed value subscribe implementation to subscriptionList

--- src/value.ts
+++ src/value.ts
@@ -2,14 +2,15 @@
 
 import changeNotification, * as changeNotificationTypes from './changeNotification';
 import * as tracking from './tracking';
+import subscriptionList, * as subscriptionTypes from './subscriptionList';
 
-export type ISubscription = changeNotificationTypes.ISubscription;
+export type ISubscription = subscriptionTypes.ISubscription;
 
 export interface IValueChangeHandler<T> {
 	(changed: IValue<T>, from: T, to: T): void;
 }
 
-export interface IValue<T> extends changeNotificationTypes.IHasValue<T> {
+export interface IValue<T> extends subscriptionTypes.IHasValue<T> {
 	write(value: T): void;
 	onChange(handler: IValueChangeHandler<T>): ISubscription;
 }
@@ -17,22 +18,11 @@ export interface IValue<T> extends changeNotificationTypes.IHasValue<T> {
 export default function<T>(initialValue: T): IValue<T> {
 	var currentValue = initialValue;
 	var id = tracking.takeNextObservableId();
-
-	interface ILinkedListItem {
-		prev: ILinkedListItem,
-		next: ILinkedListItem,
-		handler: IValueChangeHandler<T>,
-	}
+	var subscriptions: subscriptionTypes.ISubscriptions<T>;
 	
 	var self = <IValue<T>>function() {
 		tracking.recordUsage(id, self, currentValue);
 		return currentValue;
-	};
-
-	var handlersHead: ILinkedListItem = {
-		prev: null,
-		next: null,
-		handler: null
 	};
 	
 	self.write = function(newValue) {
@@ -42,44 +32,19 @@ export default function<T>(initialValue: T): IValue<T> {
 		currentValue = newValue;
 		tracking.lastWriteVersion++;
 		
-		var item = handlersHead.next;
-		while(item) {
-			item.handler(self, oldValue, newValue);
-			item = item.next;
+		if(subscriptions) {
+			subscriptions.notify(self, oldValue, newValue);
 		}
 		
 		changeNotification.notify(self, oldValue, newValue);
 	};
 	
 	self.onChange = function(handler) {
-		var item: ILinkedListItem = null;
+		if(!subscriptions) {
+			subscriptions = subscriptionList<T>();
+		}
 		
-		var subscription = {
-			enable: function() {
-				if(!item) {
-					item = {
-						prev: handlersHead,
-						next: handlersHead.next,
-						handler: handler
-					};
-					
-					handlersHead.next = item;
-				}
-			},
-			disable: function() {
-				if(item) {
-					item.prev.next = item.next;
-					if(item.next) { item.next.prev = item.prev; }
-					
-					handlersHead = item;
-					item = null;
-				}
-			}
-		};
-		
-		subscription.enable();
-		
-		return subscription;
+		return subscriptions.subscribe(handler);
 	};
 	
 	return self;
