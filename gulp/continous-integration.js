9309a88d70f9a1dd942901eb5f670a97345724eb
sergey-gerasimov <sergey.gerasimov@exigenservices.com>
2016-03-16 13:31:42 +0300

    build

--- gulp/continous-integration.js
+++ gulp/continous-integration.js
@@ -1,8 +1,3 @@
 var gulp = require('gulp');
-var isPullRequest = require('./util/is-pull-request.js');
 
-if(isPullRequest()) {
-	gulp.task('continous-integration', ['full']);
-} else {
-	gulp.task('continous-integration', ['full', 'coveralls']);
-}
+gulp.task('continous-integration', ['build', 'all-tests']);
