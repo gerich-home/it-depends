02adafffb8343fb69b195605415ca89eed1cda56
sergey-gerasimov <sergey.gerasimov@exigenservices.com>
2016-03-21 11:00:20 +0300

    tslint

---
+++ gulp/tslint.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var tslint = require('gulp-tslint');
+
+gulp.task('tslint', function() {
+    gulp.src('src/*.ts')
+        .pipe(tslint())
+        .pipe(tslint.report('prose'));
+});
