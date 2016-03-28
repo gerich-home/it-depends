e458f67fcdfe1ba8dea37de5bcf72507f3c62e35
VanDalkvist <i_van@list.ru>
2016-03-29 00:31:03 +0300

    try to push new tag

--- gulp/bower.js
+++ gulp/bower.js
@@ -3,6 +3,7 @@ var plugins = require('gulp-load-plugins')();
 var deploy = require('gulp-gh-pages');
 var sequence = plugins.sequence.use(gulp);
 var fs = require('fs');
+var Q = require('q');
 var del = require('del');
 
 var pkg = require('../package.json');
@@ -20,7 +21,9 @@ var files = {
 gulp.task('bower', sequence(
     'clean-bower-folder',
     ['copy-license', 'copy-bower-json', 'copy-bower-artifacts', 'copy-readme'],
-    'deploy-bower'
+    'clone-bower-repo-and-add-new-files'
+    // 'add-new-tag',
+    // 'push-to-repo'
 ));
 
 gulp.task('clean-bower-folder', [], function () {
@@ -48,7 +51,7 @@ gulp.task('copy-readme', [], function () {
         .pipe(gulp.dest(out.bower));
 });
 
-gulp.task('deploy-bower', [], function () {
+gulp.task('clone-bower-repo-and-add-new-files', [], function () {
 
     var bowerRepoUrl = _format('https://{identity}:{authToken}@{repo}', {
         identity: process.env.BOWER_REPO_PUBLISH_IDENTITY,
@@ -56,18 +59,36 @@ gulp.task('deploy-bower', [], function () {
         repo: process.env.BOWER_REPO
     });
 
-    var commitMessage = _format('{version} version was published', { version: pkg.version });
+    var commitMessage = _format('{version} version was published', {version: pkg.version});
 
     var deployOptions = {
         remoteUrl: bowerRepoUrl,
         origin: 'origin',
         branch: process.env.BOWER_BRANCH,
         cacheDir: out.repo,
-        message: commitMessage
+        message: commitMessage,
+        tag: process.env.APPVEYOR_REPO_TAG_NAME,
+        // push: false // don't push after committing, wait for adding tag to commit
     };
 
     return gulp.src(files.bower).pipe(deploy(deployOptions));
 });
+
+// gulp.task('add-new-tag', [], function (cb) {
+//     plugins.git.tag(process.env.APPVEYOR_REPO_TAG_NAME, out.repo, function (err) {
+//         if (err) return cb(err);
+//
+//         cb();
+//     });
+// });
+//
+// gulp.task('push-to-repo', [], function (cb) {
+//     plugins.git.push('origin', process.env.BOWER_BRANCH, {args: '--tags', cwd: out.repo/*, quiet: true*/}, function (err) {
+//         if (err) return cb(err);
+//
+//         cb();
+//     });
+// });
 
 function _addHeader(fileName) {
     return plugins.header(fs.readFileSync(fileName, 'utf8'), pkg);
