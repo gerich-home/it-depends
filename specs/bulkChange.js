915462240b5985e862d6bf5bbf935aa2bc0ea8eb
sergey-gerasimov <sergey.gerasimov@exigenservices.com>
2016-04-14 11:24:51 +0300

    test showing that general change handler works wrong with bulkChange

--- specs/bulkChange.js
+++ specs/bulkChange.js
@@ -3,17 +3,51 @@ var itDepends = require('../out/build/it-depends.js');
 var _ = require('lodash');
 
 describe('bulk change', function () {
-	var calls;
+	var initial;
 	var observableValue;
 	var otherObservableValue;
 	var computedValue;
-	var observableSubscription;
-	var otherObservableSubscription;
-	var computedSubscription;
-	var generalSubscription;
 	var atStartOfBulkChangeCallbacks;
 	var atEndOfBulkChangeCallbacks;
 	
+    var subscribeWithSpies = function() {
+        var calls = {
+            computedCount: 0,
+            observableCount: 0,
+            otherObservableCount: 0,
+            generalCount: 0
+        };
+        
+        var computedSubscription = computedValue.onChange(function(changed, from, to, args) {
+            calls.computedCount++;
+            calls.lastComputedChange = { changed: changed, from: from, to: to, args: args };
+        });
+
+        var observableSubscription = observableValue.onChange(function(changed, from, to) {
+            calls.observableCount++;
+            calls.lastObservableChange = { changed: changed, from: from, to: to };
+        });
+
+        var otherObservableSubscription = otherObservableValue.onChange(function(changed, from, to) {
+            calls.otherObservableCount++;
+            calls.lastOtherObservableChange = { changed: changed, from: from, to: to };
+        });
+
+        var generalSubscription = itDepends.onChange(function(changed, from, to) {
+            calls.generalCount++;
+            calls.lastGeneralChange = { changed: changed, from: from, to: to };
+        });
+        
+        calls.unsubscribe = function() {
+            computedSubscription.disable();
+            observableSubscription.disable();
+            otherObservableSubscription.disable();
+            generalSubscription.disable();
+        };
+    
+        return calls;
+    }
+    
 	var expectChange = function(actual, expected) {
 		expect(actual.changed).to.equal(expected.changed);
 		expect(actual.from).to.equal(expected.from);
@@ -35,61 +69,22 @@ describe('bulk change', function () {
 		    expect(actual.args).to.equal(expected.args); 
         }
 	};
-    
-	var expectLastComputedChanges = function(expected) {
-		expectChange(calls.lastComputedChange, expected);
-	};
-    
-	var expectLastObservableChanges = function(expected) {
-		expectChange(calls.lastObservableChange, expected);
-	};
-    
-	var expectLastOtherObservableChanges = function(expected) {
-		expectChange(calls.lastOtherObservableChange, expected);
-	};
-    
-	var expectLastGeneralChanges = function(expected) {
-		expectChange(calls.lastGeneralChange, expected);
-	};
 
 	beforeEach(function() {
         atStartOfBulkChangeCallbacks = [];
         atEndOfBulkChangeCallbacks = [];
-		var callsSpy = { computedCount: 0, observableCount: 0, otherObservableCount: 0, generalCount: 0 };
-		calls = callsSpy;
 
 		observableValue = itDepends.value('Bob');
 		otherObservableValue = itDepends.value('Hello');
 		computedValue = itDepends.computed(function() {
 			return otherObservableValue() + ', ' + observableValue();
 		});
-
-		computedSubscription = computedValue.onChange(function(changed, from, to, args) {
-			callsSpy.computedCount++;
-			callsSpy.lastComputedChange = { changed: changed, from: from, to: to, args: args };
-		});
-
-		observableSubscription = observableValue.onChange(function(changed, from, to) {
-			callsSpy.observableCount++;
-			callsSpy.lastObservableChange = { changed: changed, from: from, to: to };
-		});
-
-		otherObservableSubscription = otherObservableValue.onChange(function(changed, from, to) {
-			callsSpy.otherObservableCount++;
-			callsSpy.lastOtherObservableChange = { changed: changed, from: from, to: to };
-		});
-
-		generalSubscription = itDepends.onChange(function(changed, from, to) {
-			callsSpy.generalCount++;
-			callsSpy.lastGeneralChange = { changed: changed, from: from, to: to };
-		});
+        
+        initial = subscribeWithSpies();
 	});
 	
 	afterEach(function() {
-        computedSubscription.disable();
-        observableSubscription.disable();
-        otherObservableSubscription.disable();
-        generalSubscription.disable();
+        initial.unsubscribe();
 	});
     
     var atStartOfBulkChange = function(callback) {
@@ -123,9 +118,9 @@ describe('bulk change', function () {
             atEndOfBulkChange(function() {
                 expect(computedValue()).to.equal('Hello, Jack');
                 expect(observableValue()).to.equal('Jack');
-                expect(calls.computedCount).to.equal(0);
-                expect(calls.observableCount).to.equal(0);
-                expect(calls.generalCount).to.equal(0);
+                expect(initial.computedCount).to.equal(0);
+                expect(initial.observableCount).to.equal(0);
+                expect(initial.generalCount).to.equal(0);
             });
             
             doBulkChange();
@@ -134,13 +129,13 @@ describe('bulk change', function () {
 		it('should be triggered when bulk change ends', function () {
             doBulkChange();
             
-            expect(calls.computedCount).to.equal(1);
-            expect(calls.observableCount).to.equal(1);
-            expect(calls.otherObservableCount).to.equal(0);
-            expect(calls.generalCount).to.equal(1);
-			expectLastComputedChanges({ changed: computedValue.withNoArgs(), from: 'Hello, Bob', to: 'Hello, Jack', args: [] });
-			expectLastObservableChanges({ changed: observableValue, from: 'Bob', to: 'Jack' });
-			expectLastGeneralChanges({ changed: observableValue, from: 'Bob', to: 'Jack' });
+            expect(initial.computedCount).to.equal(1);
+            expect(initial.observableCount).to.equal(1);
+            expect(initial.otherObservableCount).to.equal(0);
+            expect(initial.generalCount).to.equal(1);
+			expectChange(initial.lastComputedChange, { changed: computedValue.withNoArgs(), from: 'Hello, Bob', to: 'Hello, Jack', args: [] });
+			expectChange(initial.lastObservableChange, { changed: observableValue, from: 'Bob', to: 'Jack' });
+			expectChange(initial.lastGeneralChange, { changed: observableValue, from: 'Bob', to: 'Jack' });
             expect(computedValue()).to.equal('Hello, Jack');
             expect(observableValue()).to.equal('Jack');
 		});
@@ -164,10 +159,10 @@ describe('bulk change', function () {
                     }
                 });
                 
-                expect(calls.computedCount).to.equal(0);
-                expect(calls.observableCount).to.equal(0);
-                expect(calls.generalCount).to.equal(0);
-                expect(calls.otherObservableCount).to.equal(0);
+                expect(initial.computedCount).to.equal(0);
+                expect(initial.observableCount).to.equal(0);
+                expect(initial.generalCount).to.equal(0);
+                expect(initial.otherObservableCount).to.equal(0);
                 expect(observableValue()).to.equal('Bob');
                 expect(computedValue()).to.equal('Hello, Bob');
             });
@@ -182,9 +177,9 @@ describe('bulk change', function () {
                 it('should not be triggered until bulk change ends', function () {
                     atEndOfBulkChange(function() {
                         expect(computedValue()).to.equal('Salut, Jack');
-                        expect(calls.computedCount).to.equal(0);
-                        expect(calls.generalCount).to.equal(0);
-                        expect(calls.otherObservableCount).to.equal(0);
+                        expect(initial.computedCount).to.equal(0);
+                        expect(initial.generalCount).to.equal(0);
+                        expect(initial.otherObservableCount).to.equal(0);
                     });
                     
                     doBulkChange();
@@ -193,14 +188,14 @@ describe('bulk change', function () {
                 it('should be triggered when bulk change ends', function () {
                     doBulkChange();
                     
-                    expect(calls.computedCount).to.equal(1);
-                    expect(calls.observableCount).to.equal(0);
-                    expect(calls.otherObservableCount).to.equal(1);
-                    expect(calls.generalCount).to.equal(1);
+                    expect(initial.computedCount).to.equal(1);
+                    expect(initial.observableCount).to.equal(0);
+                    expect(initial.otherObservableCount).to.equal(1);
+                    expect(initial.generalCount).to.equal(1);
                     expect(computedValue()).to.equal('Salut, Bob');
-                    expectLastComputedChanges({ changed: computedValue.withNoArgs(), from: 'Hello, Bob', to: 'Salut, Bob', args: [] });
-                    expectLastGeneralChanges({ changed: otherObservableValue, from: 'Hello', to: 'Salut' });
-                    expectLastOtherObservableChanges({ changed: otherObservableValue, from: 'Hello', to: 'Salut' });
+                    expectChange(initial.lastComputedChange, { changed: computedValue.withNoArgs(), from: 'Hello, Bob', to: 'Salut, Bob', args: [] });
+                    expectChange(initial.lastGeneralChange, { changed: otherObservableValue, from: 'Hello', to: 'Salut' });
+                    expectChange(initial.lastOtherObservableChange, { changed: otherObservableValue, from: 'Hello', to: 'Salut' });
                 });
             });
         });
@@ -214,10 +209,10 @@ describe('bulk change', function () {
 
             it('should not be triggered until bulk change ends', function () {
                 atEndOfBulkChange(function() {
-                    expect(calls.computedCount).to.equal(0);
-                    expect(calls.observableCount).to.equal(0);
-                    expect(calls.generalCount).to.equal(0);
-                    expect(calls.otherObservableCount).to.equal(0);
+                    expect(initial.computedCount).to.equal(0);
+                    expect(initial.observableCount).to.equal(0);
+                    expect(initial.generalCount).to.equal(0);
+                    expect(initial.otherObservableCount).to.equal(0);
                     expect(computedValue()).to.equal('Salut, Jack');
                 });
                 
@@ -227,15 +222,112 @@ describe('bulk change', function () {
             it('should be triggered when bulk change ends', function () {
                 doBulkChange();
                     
-                expect(calls.computedCount).to.equal(1);
-                expect(calls.observableCount).to.equal(1);
-                expect(calls.otherObservableCount).to.equal(1);
-                expect(calls.generalCount).to.equal(2);
-                expectLastComputedChanges({ changed: computedValue.withNoArgs(), from: 'Hello, Bob', to: 'Salut, Jack', args: [] });
-                expectLastObservableChanges({ changed: observableValue, from: 'Bob', to: 'Jack' });
-                expectLastOtherObservableChanges({ changed: otherObservableValue, from: 'Hello', to: 'Salut' });
+                expect(initial.computedCount).to.equal(1);
+                expect(initial.observableCount).to.equal(1);
+                expect(initial.otherObservableCount).to.equal(1);
+                expect(initial.generalCount).to.equal(2);
+                expectChange(initial.lastComputedChange, { changed: computedValue.withNoArgs(), from: 'Hello, Bob', to: 'Salut, Jack', args: [] });
+                expectChange(initial.lastObservableChange, { changed: observableValue, from: 'Bob', to: 'Jack' });
+                expectChange(initial.lastOtherObservableChange, { changed: otherObservableValue, from: 'Hello', to: 'Salut' });
+            });
+        
+            context('when additional subscriptions are made in bulk change block', function() {
+                var additional;
+                
+                beforeEach(function() {
+                    atStartOfBulkChange(function() {
+                        additional = subscribeWithSpies();
+                    });
+                });
+            
+                context('when unsubscribing at the end of bulk change block', function() {
+                    beforeEach(function() {
+                        atEndOfBulkChange(function() {
+                            additional.unsubscribe();
+                        });
+                    });
+                    
+                    it('inner subscriptions should not ever be triggered', function() {
+                        doBulkChange();
+                        
+                        expect(additional.computedCount).to.equal(0);
+                        expect(additional.observableCount).to.equal(0);
+                        expect(additional.otherObservableCount).to.equal(0);
+                        expect(additional.generalCount).to.equal(0);
+                    });
+                });
+            
+                context('when not unsubscribing at the end of bulk change block', function() {
+                    afterEach(function() {
+                        additional.unsubscribe();
+                    });
+                    
+                    it('inner subscriptions should not ever be triggered', function() {
+                        doBulkChange();
+                        
+                        expect(additional.computedCount).to.equal(0);
+                        expect(additional.observableCount).to.equal(0);
+                        expect(additional.otherObservableCount).to.equal(0);
+                        expect(additional.generalCount).to.equal(0);
+                            
+                        expect(initial.computedCount).to.equal(1);
+                        expect(initial.observableCount).to.equal(1);
+                        expect(initial.otherObservableCount).to.equal(1);
+                        expect(initial.generalCount).to.equal(2);
+                        expectChange(initial.lastComputedChange, { changed: computedValue.withNoArgs(), from: 'Hello, Bob', to: 'Salut, Jack', args: [] });
+                        expectChange(initial.lastObservableChange, { changed: observableValue, from: 'Bob', to: 'Jack' });
+                        expectChange(initial.lastOtherObservableChange, { changed: otherObservableValue, from: 'Hello', to: 'Salut' });
+                    });
+            
+                    context('when changing observables after additional subscriptions', function() {
+                        beforeEach(function() {
+                            atStartOfBulkChange(function() {
+                                observableValue.write('Peter');
+                                otherObservableValue.write('Bonjour');
+                            });
+                        });
+                    
+                        it('inner subscriptions should not be triggered until bulk change ends', function() {
+                            atEndOfBulkChange(function() {
+                                expect(additional.computedCount).to.equal(0);
+                                expect(additional.observableCount).to.equal(0);
+                                expect(additional.otherObservableCount).to.equal(0);
+                                expect(additional.generalCount).to.equal(0);
+                            });
+                            
+                            doBulkChange();
+                            
+                            expect(initial.computedCount).to.equal(1);
+                            expect(initial.observableCount).to.equal(1);
+                            expect(initial.otherObservableCount).to.equal(1);
+                            expect(initial.generalCount).to.equal(2);
+                            expectChange(initial.lastComputedChange, { changed: computedValue.withNoArgs(), from: 'Hello, Bob', to: 'Bonjour, Peter', args: [] });
+                            expectChange(initial.lastObservableChange, { changed: observableValue, from: 'Bob', to: 'Peter' });
+                            expectChange(initial.lastOtherObservableChange, { changed: otherObservableValue, from: 'Hello', to: 'Bonjour' });
+                        });
+                    
+                        it('inner subscriptions should be triggered when bulk change ends', function() {
+                            doBulkChange();
+                            
+                            expect(initial.computedCount).to.equal(1);
+                            expect(initial.observableCount).to.equal(1);
+                            expect(initial.otherObservableCount).to.equal(1);
+                            expect(initial.generalCount).to.equal(2);
+                            expectChange(initial.lastComputedChange, { changed: computedValue.withNoArgs(), from: 'Hello, Bob', to: 'Bonjour, Peter', args: [] });
+                            expectChange(initial.lastObservableChange, { changed: observableValue, from: 'Bob', to: 'Peter' });
+                            expectChange(initial.lastOtherObservableChange, { changed: otherObservableValue, from: 'Hello', to: 'Bonjour' });
+                            
+                            expect(additional.computedCount).to.equal(1);
+                            expect(additional.observableCount).to.equal(1);
+                            expect(additional.otherObservableCount).to.equal(1);
+                            expect(additional.generalCount).to.equal(2);
+                            expectChange(additional.lastComputedChange, { changed: computedValue.withNoArgs(), from: 'Salut, Jack', to: 'Bonjour, Peter', args: [] });
+                            expectChange(additional.lastObservableChange, { changed: observableValue, from: 'Jack', to: 'Peter' });
+                            expectChange(additional.lastOtherObservableChange, { changed: otherObservableValue, from: 'Salut', to: 'Bonjour' });
+                        });
+                    });
+                });
             });
         });
-    
     });
 });
